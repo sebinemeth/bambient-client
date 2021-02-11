fd42548f47a17e8dd146c85081c43063f4de458e
Sebi Nemeth <sebi.nemeth@apicore.de>
2021-02-11 15:35:49 +0100

    sw

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,33 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'bAmbient',
+    themeColor: '#4c0e5f',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      name: 'Budapest Ambient Display',
+      short_name: 'bAmbient',
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+      skipWaiting: true,
+      clientsClaim: true,
+    }
+  }
 }
